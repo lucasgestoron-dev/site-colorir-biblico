446166a36eee8d6bc3fd0419a063ca04a6355eb4
lucasgestoron-dev <lucasgestor.on@gmail.com>
2026-09-18 02:47:23 -0300

    Build Mundo dos Blocos mathematics landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Matematica from "./pages/Matematica";
-import BlocoDosMundos from "./pages/BlocoDosMundos";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,12 +18,11 @@ const App = () => (
         <Routes>
           {/* Rota Principal (Home) */}
           <Route path="/" element={<Index />} />
-
-          {/* Produtos */}
+          
+          {/* Rota Específica (Futuro padrão) */}
           <Route path="/biblicos" element={<Index />} />
           <Route path="/matematica" element={<Matematica />} />
-          <Route path="/bloco-dos-mundos" element={<BlocoDosMundos />} />
-
+          
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
