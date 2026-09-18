5897faf13d013745b61b3051003e069fb0f01691
lucasgestoron-dev <lucasgestor.on@gmail.com>
2026-09-18 02:56:14 -0300

    Publish new landing page at /bloco-dos-mundos

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Matematica from "./pages/Matematica";
-import BlocoDosMundos from "./pages/BlocoDosMundos";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -23,7 +22,7 @@ const App = () => (
           {/* Produtos */}
           <Route path="/biblicos" element={<Index />} />
           <Route path="/matematica" element={<Matematica />} />
-          <Route path="/bloco-dos-mundos" element={<BlocoDosMundos />} />
+          <Route path="/bloco-dos-mundos" element={<Matematica />} />
 
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
